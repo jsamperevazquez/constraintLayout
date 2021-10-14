1569e7c94afdca1f608f9128ae960d8fde2cc78e
JSampereVázquez <jsamperevazquez@danielcastelao.org>
2021-10-14 14:15:35 +0200

    Creada función para botón

--- app/src/main/java/com/cursoandroid/constraintlayout/MainActivity.kt
+++ app/src/main/java/com/cursoandroid/constraintlayout/MainActivity.kt
@@ -2,10 +2,18 @@ package com.cursoandroid.constraintlayout
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.service.autofill.Validators.not
+import android.view.View
+import android.widget.Button
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val textViewStart = findViewById<TextView>(R.id.textView)
+        findViewById<Button>(R.id.button).setOnClickListener {
+            textViewStart.visibility = View.VISIBLE
+        }
     }
 }
